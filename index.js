1bda86ed03de851bdeca9ec74d516afa26266713
Pain-web452 <mithundas446046@gmail.com>
2025-09-26 20:42:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ try {
 }
 
 // ✅ Group Info (change these)
-const GROUP_THREAD_ID = "25272934338957475";        // Group ka ID
-const LOCKED_GROUP_NAME = "SALMAN + MANO RANDI + SUKOON RANDI + IMMU + SARKAR GANG KI MAA CHODNE WALA HERE 💀";     // Locked name
+const GROUP_THREAD_ID = "1482229652810042";        // Group ka ID
+const LOCKED_GROUP_NAME = "PRADEEP OMM SE CHUD K KLP RAHA H  💀";     // Locked name
 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
 const app = express();
